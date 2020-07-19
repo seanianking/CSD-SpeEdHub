fbaad7c3502f50b6f4a1460870f95fc449feab36
seanianking <seanianking@gmail.com>
2020-07-19 12:02:28 -0600

    added all remaining lists into dropdowns and added datatype name to each option.

--- client/src/components/formComponents/PersonalInfoForm/PersonalInfoForm.js
+++ client/src/components/formComponents/PersonalInfoForm/PersonalInfoForm.js
@@ -9,7 +9,6 @@ function PersonalInfoForm() {
         email: '',
         school: '',
         teacher: '',
-        date: '',
         student: '',
         studentID: '',
         dateOfBirth: '',
@@ -56,7 +55,7 @@ function PersonalInfoForm() {
                         <div class="field is-expanded">
                             <div class="field">
                                 <p class="control is-expanded">
-                                    <input class="input" name='dateOfAudit' type="date" placeholder="Student Date of Birth" />
+                                    <input class="input" name='dateOfAudit' type="date" />
                                 </p>
                             </div>
                             <p class='help'>Date of Audit</p>
@@ -69,20 +68,25 @@ function PersonalInfoForm() {
                         <label class="label">Student information</label>
                     </div>
                     <div class="field-body">
+                    <div class="field">
+                            <p class="control ">
+                                <input class="input" name='teacher' type="text" placeholder="Teacher" />
+                            </p>
+                        </div>
                         <div class="field">
-                            <p class="control is-expanded">
+                            <p class="control ">
                                 <input class="input" name='student' type="text" placeholder="Student name" />
                             </p>
                         </div>
                         <div class="field">
-                            <p class="control is-expanded">
+                            <p class="control ">
                                 <input class="input" name='studentID' type="text" placeholder="Student ID" />
                             </p>
                         </div>
                         <div class="field is-expanded">
                             <div class="field">
                                 <p class="control is-expanded">
-                                    <input class="input" name='dateOfBirth' type="date" placeholder="Student Date of Birth" />
+                                    <input class="input" name='dateOfBirth' type="date"  />
                                 </p>
                             </div>
                             <p class='help'>Date of birth</p>
@@ -99,22 +103,54 @@ function PersonalInfoForm() {
                         <div class="field">
                             <div class="control">
                                 <div class="select is-fullwidth">
-                                    <select>
+                                    <select name='school'>
                                         <option>School</option>
-                                        <option>Preschool</option>
-                                        <option>Kindergarten</option>
-                                        <option>1</option>
-                                        <option>2</option>
-                                        <option>3</option>
-                                        <option>4</option>
-                                        <option>5</option>
-                                        <option>6</option>
-                                        <option>7</option>
-                                        <option>8</option>
-                                        <option>9</option>
-                                        <option>10</option>
-                                        <option>11</option>
-                                        <option>12</option>
+                                        <option>{'Alta View [E]'}</option>
+                                        <option>{'Altara [E]'}</option>
+                                        <option>{'BellView [E]'}</option>
+                                        <option>{'Bella Vista [E]'}</option>
+                                        <option>{'Brookwood [E]'}</option>
+                                        <option>{'Butler [E]'}</option>
+                                        <option>{'Canyon view [E]'}</option>
+                                        <option>{'Copperview [E]'}</option>
+                                        <option>{'Crescent [E]'}</option>
+                                        <option>{'Draper [E]'}</option>
+                                        <option>{'East Midvale [E]'}</option>
+                                        <option>{'East Sandy [E]'}</option>
+                                        <option>{'Edgemont [E]'}</option>
+                                        <option>{'Granite [E]'}</option>
+                                        <option>{'Lone Peak [E]'}</option>
+                                        <option>{'Midvale [E]'}</option>
+                                        <option>{'Midvalley [E]'}</option>
+                                        <option>{'Oak Hollow [E]'}</option>
+                                        <option>{'Oakdale [E]'}</option>
+                                        <option>{'Park Lane [E]'}</option>
+                                        <option>{'Peruvian Park [E]'}</option>
+                                        <option>{'Quail Hollow [E]'}</option>
+                                        <option>{'Ridgecrest [E]'}</option>
+                                        <option>{'Sandy [E]'}</option>
+                                        <option>{'Silver Mesa [E]'}</option>
+                                        <option>{'Sprucewood [E]'}</option>
+                                        <option>{'Sunrise [E]'}</option>
+                                        <option>{'Willow Canyon [E]'}</option>
+                                        <option>{'Willow Springs [E]'}</option>
+                                        <option>{'Albion [M]'}</option>
+                                        <option>{'Butler [M]'}</option>
+                                        <option>{'Draper Park [M]'}</option>
+                                        <option>{'Eastmont [M]'}</option>
+                                        <option>{'Indian Hills [M]'}</option>
+                                        <option>{'Midvale [M]'}</option>
+                                        <option>{'Mount Jordan [M]'}</option>
+                                        <option>{'Union [M]'}</option>
+                                        <option>{'Alta [H]'}</option>
+                                        <option>{'Brighton [H]'}</option>
+                                        <option>{'Corner Canyon [H]'}</option>
+                                        <option>{'Hillcrest [H]'}</option>
+                                        <option>{'Diamond Ridge [H]'}</option>
+                                        <option>{'Jordan [H]'}</option>
+                                        <option>{'Jordan Valley [H]'}</option>
+                                        <option>{'South Park [S]'}</option>
+                                        <option>{'Transition Academy [S]'}</option>
                                     </select>
                                 </div>
                             </div>
@@ -122,7 +158,7 @@ function PersonalInfoForm() {
                         <div class="field ">
                             <div class="control">
                                 <div class="select is-fullwidth">
-                                    <select>
+                                    <select name='grade'>
                                         <option>Grade</option>
                                         <option>Preschool</option>
                                         <option>Kindergarten</option>
@@ -138,6 +174,7 @@ function PersonalInfoForm() {
                                         <option>10</option>
                                         <option>11</option>
                                         <option>12</option>
+                                        <option>Post-High</option>
                                     </select>
                                 </div>
                             </div>
@@ -145,7 +182,7 @@ function PersonalInfoForm() {
                         <div class="field ">
                             <div class="control">
                                 <div class="select is-fullwidth">
-                                    <select>
+                                    <select name='race'>
                                         <option>Race</option>
                                         <option>White</option>
                                         <option>Hispanic</option>
@@ -159,7 +196,7 @@ function PersonalInfoForm() {
                         <div class="field is-expanded">
                             <div class="control">
                                 <div class="select is-fullwidth">
-                                    <select>
+                                    <select name='classification'>
                                         <option>Classification</option>
                                         <option>Autism</option>
                                         <option>Developmental Delay</option>
