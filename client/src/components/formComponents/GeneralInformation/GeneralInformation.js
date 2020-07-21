7e0c1152207bb5f6a2d6db43f4ff6fc24af581bd
seanianking <seanianking@gmail.com>
2020-07-21 12:20:31 -0600

    general formatting changes made

--- client/src/components/formComponents/GeneralInformation/GeneralInformation.js
+++ client/src/components/formComponents/GeneralInformation/GeneralInformation.js
@@ -1,10 +1,49 @@
 import React from 'react'
+import useForm from '../../../hooks/useForm'
 import '../form.css'
 
 function GeneralInformation() {
+
+    const initialValues = {
+        auditorName: '',
+        email: '',
+        school: '',
+        teacher: '',
+        student: '',
+        studentID: '',
+        dateOfBirth: '',
+        grade: 0,
+        race: '',
+        classification: '',
+        dateOfAudit: '',
+    };
+
+    const {
+        values,
+        errors,
+        touched,
+        handleChange,
+        handleBlur,
+        handleSubmit
+    } = useForm({
+        initialValues, onSubmit: values => console.log({ values })
+    });
+
     return (
-        <div>
-            
+        <div className>
+            <form onSubmit={handleSubmit}>
+                <div class="field is-horizontal">
+                    <div class="header">
+                        <h1 style={{fontSize:'40px'}} class="label border-bottom">1.0 General Information</h1>
+                    </div>
+                </div>
+            <div class='columns'>
+                <div class='column'>
+
+                </div>
+            </div>
+                
+            </form>
         </div>
     )
 }
