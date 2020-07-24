522061976db87303c790d0f06175e97caf7ee793
seanianking <seanianking@gmail.com>
2020-07-23 18:29:58 -0600

    formatting section questions

--- client/src/components/formComponents/GeneralInformation/GeneralInformation.js
+++ client/src/components/formComponents/GeneralInformation/GeneralInformation.js
@@ -5,17 +5,15 @@ import '../form.css'
 function GeneralInformation() {
 
     const initialValues = {
-        auditorName: '',
-        email: '',
-        school: '',
-        teacher: '',
-        student: '',
-        studentID: '',
-        dateOfBirth: '',
-        grade: 0,
-        race: '',
-        classification: '',
-        dateOfAudit: '',
+        generalone: '',
+        generaltwo: '',
+        generalthree: '',
+        generalfour: '',
+        generalfive: '',
+        generalsix: '',
+        generalseven: '',
+        generaleight: '',
+        generaltype: ''
     };
 
     const {
@@ -33,18 +31,57 @@ function GeneralInformation() {
         <div className>
             <form onSubmit={handleSubmit}>
                 <div class="field is-horizontal">
-                    <div class="header">
-                        <h1 style={{fontSize:'40px'}} class="label border-bottom">1.0 General Information</h1>
+                    <div class='field'>
+                        <div class="header">
+                            <h1 style={{ fontSize: '40px' }} class="label border-bottom">1.0 General Information</h1>
+                        </div>
                     </div>
                 </div>
-            <div class='columns'>
-                <div class='column'>
-
+                <br />
+                <div class="field is-horizontal">
+                    <div class='field-body'>
+                        <div class='field'>
+                            <div class='question'>
+                                <p style={{fontWeight: 'bold'}}>{'Is the file organized in the new format (1.14)?'}</p>
+                            </div>
+                        </div>
+                        <div class='field'>
+                            <div class='control is-expanded'>
+                                <div class="select is-fullwidth">
+                                    <select>
+                                        <option>Yes/No</option>
+                                        <option>Yes</option>
+                                        <option>No</option>
+                                    </select>
+                                </div>
+                            </div>
+                        </div>
+                        <div class='field'>
+                            <div class='control is-expanded'>
+                                <div class="select is-fullwidth">
+                                    <select>
+                                        <option>Findings</option>
+                                        <option>Yes</option>
+                                        <option>No</option>
+                                    </select>
+                                </div>
+                            </div>
+                        </div>
+                        <div class='field'>
+                            <div class='control is-expanded'>
+                                <div class="select is-fullwidth">
+                                    <select>
+                                        <option>Correction</option>
+                                        <option>Yes</option>
+                                        <option>No</option>
+                                    </select>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
                 </div>
-            </div>
-                
             </form>
-        </div>
+        </div >
     )
 }
 
