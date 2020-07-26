30113464b24c8bad0cd37df4f2a379c4d3c75568
seanianking <seanianking@gmail.com>
2020-07-26 03:30:46 -0600

    remaining questions added to geninfo section. Needs dropdown lists

--- client/src/components/formComponents/GeneralInformation/GeneralInformation.js
+++ client/src/components/formComponents/GeneralInformation/GeneralInformation.js
@@ -40,8 +40,8 @@ function GeneralInformation() {
                 <br />
                 <div class="field is-horizontal">
                     <div class='field-body'>
-                        <div class='field'>
-                            <div class='question'>
+                        <div class='field question'>
+                            <div class=''>
                                 <p style={{fontWeight: 'bold'}}>{'Is the file organized in the new format (1.14)?'}</p>
                             </div>
                         </div>
@@ -78,6 +78,281 @@ function GeneralInformation() {
                                 </div>
                             </div>
                         </div>
+                    </div>
+                </div>
+                <div class="field is-horizontal">
+                    <div class='field-body'>
+                        <div class='field question'>
+                            <div class=''>
+                                <p style={{fontWeight: 'bold'}}>{'Primary Language?'}</p>
+                            </div>
+                        </div>
+                        <div class='field'>
+                            <div class='control is-expanded'>
+                                <div class="select is-fullwidth">
+                                    <select>
+                                        <option>Yes/No</option>
+                                        <option>Yes</option>
+                                        <option>No</option>
+                                    </select>
+                                </div>
+                            </div>
+                        </div>
+                        <div class='field'>
+                            <div class='control is-expanded'>
+                                <div class="select is-fullwidth">
+                                    <select>
+                                        <option>Findings</option>
+                                        <option>Yes</option>
+                                        <option>No</option>
+                                    </select>
+                                </div>
+                            </div>
+                        </div>
+                        <div class='field'>
+                            <div class='control is-expanded'>
+                                <div class="select is-fullwidth">
+                                    <select>
+                                        <option>Correction</option>
+                                        <option>Yes</option>
+                                        <option>No</option>
+                                    </select>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="field is-horizontal">
+                    <div class='field-body'>
+                        <div class='field question'>
+                            <div class=''>
+                                <p style={{fontWeight: 'bold'}}>{'Is the home language different than the primary language? (2.18) (3.17)'}</p>
+                            </div>
+                        </div>
+                        <div class='field'>
+                            <div class='control is-expanded'>
+                                <div class="select is-fullwidth">
+                                    <select>
+                                        <option>Yes/No</option>
+                                        <option>Yes</option>
+                                        <option>No</option>
+                                    </select>
+                                </div>
+                            </div>
+                        </div>
+                        <div class='field'>
+                            <div class='control is-expanded'>
+                                <div class="select is-fullwidth">
+                                    <select>
+                                        <option>Findings</option>
+                                        <option>Yes</option>
+                                        <option>No</option>
+                                    </select>
+                                </div>
+                            </div>
+                        </div>
+                        <div class='field'>
+                            <div class='control is-expanded'>
+                                <div class="select is-fullwidth">
+                                    <select>
+                                        <option>Correction</option>
+                                        <option>Yes</option>
+                                        <option>No</option>
+                                    </select>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="field is-horizontal">
+                    <div class='field-body'>
+                        <div class='field question'>
+                            <div class=''>
+                                <p style={{fontWeight: 'bold'}}>{'Is there a signed Medicaid form (attached to front of file)? (1.1)'}</p>
+                            </div>
+                        </div>
+                        <div class='field'>
+                            <div class='control is-expanded'>
+                                <div class="select is-fullwidth">
+                                    <select>
+                                        <option>Yes/No</option>
+                                        <option>Yes</option>
+                                        <option>No</option>
+                                    </select>
+                                </div>
+                            </div>
+                        </div>
+                        <div class='field'>
+                            <div class='control is-expanded'>
+                                <div class="select is-fullwidth">
+                                    <select>
+                                        <option>Findings</option>
+                                        <option>Yes</option>
+                                        <option>No</option>
+                                    </select>
+                                </div>
+                            </div>
+                        </div>
+                        <div class='field'>
+                            <div class='control is-expanded'>
+                                <div class="select is-fullwidth">
+                                    <select>
+                                        <option>Correction</option>
+                                        <option>Yes</option>
+                                        <option>No</option>
+                                    </select>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="field is-horizontal">
+                    <div class='field-body'>
+                        <div class='field question'>
+                            <div class=''>
+                                <p style={{fontWeight: 'bold'}}>{'Is the box giving consent on the medicaid form checked? (1.2)'}</p>
+                            </div>
+                        </div>
+                        <div class='field'>
+                            <div class='control is-expanded'>
+                                <div class="select is-fullwidth">
+                                    <select>
+                                        <option>Yes/No</option>
+                                        <option>Yes</option>
+                                        <option>No</option>
+                                    </select>
+                                </div>
+                            </div>
+                        </div>
+                        <div class='field'>
+                            <div class='control is-expanded'>
+                                <div class="select is-fullwidth">
+                                    <select>
+                                        <option>Findings</option>
+                                        <option>Yes</option>
+                                        <option>No</option>
+                                    </select>
+                                </div>
+                            </div>
+                        </div>
+                        <div class='field'>
+                            <div class='control is-expanded'>
+                                <div class="select is-fullwidth">
+                                    <select>
+                                        <option>Correction</option>
+                                        <option>Yes</option>
+                                        <option>No</option>
+                                    </select>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="field is-horizontal">
+                    <div class='field-body'>
+                        <div class='field question'>
+                            <div class=''>
+                                <p style={{fontWeight: 'bold'}}>{'Initial placement in file? (1.3)'}</p>
+                            </div>
+                        </div>
+                        <div class='field'>
+                            <div class='control is-expanded'>
+                                <div class="select is-fullwidth">
+                                    <select>
+                                        <option>Yes/No</option>
+                                        <option>Yes</option>
+                                        <option>No</option>
+                                    </select>
+                                </div>
+                            </div>
+                        </div>
+                        <div class='field'>
+                            <div class='control is-expanded'>
+                                <div class="select is-fullwidth">
+                                    <select>
+                                        <option>Findings</option>
+                                        <option>Yes</option>
+                                        <option>No</option>
+                                    </select>
+                                </div>
+                            </div>
+                        </div>
+                        <div class='field'>
+                            <div class='control is-expanded'>
+                                <div class="select is-fullwidth">
+                                    <select>
+                                        <option>Correction</option>
+                                        <option>Yes</option>
+                                        <option>No</option>
+                                    </select>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="field is-horizontal">
+                    <div class='field-body'>
+                        <div class='field question'>
+                            <div class=''>
+                                <p style={{fontWeight: 'bold'}}>{'Is the box on the initial placement form checked? (1.4)'}</p>
+                            </div>
+                        </div>
+                        <div class='field'>
+                            <div class='control is-expanded'>
+                                <div class="select is-fullwidth">
+                                    <select>
+                                        <option>Yes/No</option>
+                                        <option>Yes</option>
+                                        <option>No</option>
+                                    </select>
+                                </div>
+                            </div>
+                        </div>
+                        <div class='field'>
+                            <div class='control is-expanded'>
+                                <div class="select is-fullwidth">
+                                    <select>
+                                        <option>Findings</option>
+                                        <option>Yes</option>
+                                        <option>No</option>
+                                    </select>
+                                </div>
+                            </div>
+                        </div>
+                        <div class='field'>
+                            <div class='control is-expanded'>
+                                <div class="select is-fullwidth">
+                                    <select>
+                                        <option>Correction</option>
+                                        <option>Yes</option>
+                                        <option>No</option>
+                                    </select>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="field is-horizontal">
+                    <div class='field-body'>
+                        <div class='field question'>
+                            <div class=''>
+                                <p style={{fontWeight: 'bold'}}>{'Evaluation type'}</p>
+                            </div>
+                        </div>
+                        <div class='field'>
+                            <div class='control is-expanded'>
+                                <div class="select is-fullwidth">
+                                    <select>
+                                        <option>Select</option>
+                                        <option>{'Initial(age 3-17)'}</option>
+                                        <option>{'Initial(age 18-21)'}</option>
+                                        <option>{'Re-eval(age 3-17)'}</option>
+                                        <option>{'Re-eval(age 18-21)'}</option>
+                                        <option>{'Re-eval(age 8-21 with guardian)'}</option>
+                                    </select>
+                                </div>
+                            </div>
+                        </div>                
                     </div>
                 </div>
             </form>
